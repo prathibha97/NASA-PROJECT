60044d544ce51aca6e964a60ffcba30dbbe6c47c
Prathibha <prsthibha@gmail.com>
2022-09-07 15:41:32 +0530

    DELETE launches Aborting Launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
+const {
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchId,
+  abortLaunchById,
+} = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
@@ -28,4 +33,16 @@ function httpAddNewLaunch(req, res) {
   return res.status(201).json(launch);
 }
 
-module.exports = { httpGetAllLaunches, httpAddNewLaunch };
+function httpAbortLaunch(req, res) {
+  const launchID = Number(req.params.id);
+
+  if (!existsLaunchId(launchID)) {
+    return res.status(404).json({
+      error: "Launch not found",
+    });
+  }
+  const aborted = abortLaunchById(launchID);
+  return res.status(200).json(aborted);
+}
+
+module.exports = { httpGetAllLaunches, httpAddNewLaunch, httpAbortLaunch };
