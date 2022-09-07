e060c107a8c1cc254f2b91e5e0d8759f9e0783df
Prathibha <prsthibha@gmail.com>
2022-09-07 12:33:59 +0530

    Add POST launch functionality

--- server/src/app.js
+++ server/src/app.js
@@ -16,10 +16,10 @@ app.use(
 app.use(morgan("combined"));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public", 'build')));
+app.use(express.static(path.join(__dirname, "..", "public", "build")));
 
-app.use(planetsRouter);
-app.use(launchesRouter);
+app.use("/planets", planetsRouter);
+app.use("/launches", launchesRouter);
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 // });
--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Explorer 101",
@@ -17,4 +19,17 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
-module.exports = { getAllLaunches };
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+      customers: ["Prathibha", "NASA"],
+      upcoming: true,
+      success: true,
+    })
+  );
+}
+
+module.exports = { getAllLaunches, addNewLaunch };
