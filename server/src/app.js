3cb877c05cbdebef6f23bf593fba0016eda83f8b
Prathibha <prsthibha@gmail.com>
2022-09-07 10:10:46 +0530

    add morgan npm package

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const morgan = require("morgan");
 const { planetsRouter } = require("./routes/planets/planets.router");
 
 const app = express();
@@ -10,6 +11,9 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
+app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
