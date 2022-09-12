17c86fb0ce6658362c0ad330cdda13bb38c0d7c2
Prathibha <prsthibha@gmail.com>
2022-09-12 18:09:50 +0530

    Bug fix - 404 when refreshing the browser

--- server/src/app.js
+++ server/src/app.js
@@ -20,9 +20,9 @@ app.use(express.static(path.join(__dirname, "..", "..", "client", "build")));
 
 app.use("/planets", planetsRouter);
 app.use("/launches", launchesRouter);
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "..", "public", "build", "index.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "client", "build", "index.html"));
+});
 
 module.exports = app;
 
