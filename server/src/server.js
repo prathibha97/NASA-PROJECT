3a612b1bb96e9eb7d624473568d497caee7678af
Prathibha <prsthibha@gmail.com>
2022-09-12 18:01:21 +0530

    Update tests for mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,26 +1,14 @@
 const http = require("http");
-const mongoose = require("mongoose");
 const app = require("./app");
+const { mongoConnect } = require("./services/mongo");
 const { loadPlanetData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URI =
-  "mongodb://localhost:27017/nasa";
-
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URI);
-
+  await mongoConnect();
   await loadPlanetData();
 
   server.listen(PORT, () => {
@@ -29,5 +17,3 @@ async function startServer() {
 }
 
 startServer();
-
-// mongodb+srv://nasa-api:VWKYTXUeSnFxdTSc@nasacluster.2ycexic.mongodb.net/nasa
---
+++ server/src/services/mongo.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const MONGO_URI = "mongodb://localhost:27017/nasa";
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URI);
+}
+
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
+
+module.exports = { mongoConnect, mongoDisconnect };
+
+// mongodb+srv://nasa-api:VWKYTXUeSnFxdTSc@nasacluster.2ycexic.mongodb.net/nasa
